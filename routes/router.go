83969988ee9116452627747ca471f4243fd13e9c
shem958 <shemgikunda37@gmail.com>
2025-08-18 22:44:26 +0300

    Add analytics routes to the pregnancy checkup section of the API

--- routes/router.go
+++ routes/router.go
@@ -37,6 +37,7 @@ func SetupRouter() *gin.Engine {
 	// ✅ Pregnancy Checkup Routes
 	RegisterPregnancyCheckupRoutes(api, config.DB)
 	RegisterPostpartumCheckupRoutes(api)
+	RegisterAnalyticsRoutes(api)
 
 	// ✅ Block/Mute routes (protected)
 	api.POST("/block", middleware.AuthMiddleware(), controllers.BlockOrMuteUser)
